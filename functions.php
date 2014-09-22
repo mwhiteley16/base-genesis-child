14aebd2f4cc7fe65fcc9a0e1605aaecf4ee726ba
Matt Whiteley <matt@whiteleydesigns.com>
2014-09-22 11:13:29 -0400

    Functions.php cleanup
    Cleanup functions.php.  Add in default page-template removal.

--- functions.php
+++ functions.php
@@ -30,13 +30,31 @@ function mw_load_scripts() {
 	wp_enqueue_script('jquery');
 }
 
-// Add Open Sans Font
+// Enqueue custom styles (google fonts, font-awesome, etc...)
 add_action( 'wp_enqueue_scripts', 'mw_enqueue_google_fonts' );
 function mw_enqueue_google_fonts() {
      wp_enqueue_style( 'google-font-open-sans', '//fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,700,800,600', array(), CHILD_THEME_VERSION );
 }
 
-// Edit the header layout
+//Remove default Genesis page templates (they are no good and I will never use them and they confuse clients...)
+function wd_remove_genesis_page_templates( $page_templates ) {
+	unset( $page_templates['page_archive.php'] );
+	unset( $page_templates['page_blog.php'] );
+	return $page_templates;
+}
+add_filter( 'theme_page_templates', 'wd_remove_genesis_page_templates' );
+
+
+//Remove default genesis sidebar
+remove_action( 'genesis_sidebar', 'genesis_do_sidebar' );
+unregister_sidebar( 'header-right' );
+unregister_sidebar( 'sidebar' );
+unregister_sidebar( 'sidebar-alt' );
+unregister_sidebar( 'footer-1' );
+unregister_sidebar( 'footer-2' );
+unregister_sidebar( 'footer-3' );
+
+//Edit the header layout
 remove_action( 'genesis_header', 'genesis_do_header' );
 add_action( 'genesis_header', 'genesis_do_new_header' );
 function genesis_do_new_header() {
@@ -50,20 +68,12 @@ function genesis_do_new_footer() {
      get_template_part( 'inc/footer' );
 }
 
+//Don't use this unless we really need to remove all the wrapping <p>'s WP creates
 //Stop wordpress from wrapping everything in paragraph tags
 //remove_filter( 'the_content', 'wpautop' );
 //remove_filter( 'the_excerpt', 'wpautop' );
 
-//Remove default genesis sidebar
-remove_action( 'genesis_sidebar', 'genesis_do_sidebar' );
-unregister_sidebar( 'header-right' );
-unregister_sidebar( 'sidebar' );
-unregister_sidebar( 'sidebar-alt' );
-unregister_sidebar( 'footer-1' );
-unregister_sidebar( 'footer-2' );
-unregister_sidebar( 'footer-3' );
-
-// Adding custom Favicon
+// Adding custom Favicon (only use if favicon not added natively)
 //add_filter( 'genesis_pre_load_favicon', 'custom_favicon' );
 //function custom_favicon( $favicon_url ) {
 //	return 'urltofavicon';
