164e71a650669b667dd450d8e4ca9a6a258d7f20
Matt Whiteley <matt@whiteleydesigns.com>
2014-09-25 12:19:59 -0400

    Remove un-needed thumbanil section.
    Used on past site - no need for base theme.

--- page_base.php
+++ page_base.php
@@ -14,14 +14,7 @@ Template Name: Base Page Template
 
                <article class="main-content <?php echo the_title(); ?>">    
           
-                    <?php if( has_post_thumbnail() ) : ?>
-                         <div class="page-header-img-con">
-                              <?php the_post_thumbnail(); ?>
-                              <h1 class="page-header"><span><?php the_title(); ?></span></h1>
-                         </div><!-- /page-header-img-con -->
-                    <?php else : ?>
-                         <h1 class="page-header"><span><?php the_title(); ?></span></h1>
-                    <?php endif; ?>
+                    <h1 class="page-header"><span><?php the_title(); ?></span></h1>
 
                     <div class="page-content">
                          <?php the_content(); ?>
