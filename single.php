22231314b2fd7e46d1d664e68d0bdf73685bd393
Matt Whiteley <matt@whiteleydesigns.com>
2015-02-16 11:39:21 -0500

    Add single post layout to match child theme wrappers.

---
+++ single.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+<section class="page-interior">
+
+     <div class="interior-wrap">
+
+          <?php genesis_loop(); ?>
+
+     </div><!-- /interior-wrap -->
+
+</section><!-- /page-interior -->
+
+<?php get_footer(); ?>
