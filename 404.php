b05515870460c0c12a46ff459b5e9f28e3c581fe
Matt Whiteley <matt@whiteleydesigns.com>
2015-02-16 11:37:22 -0500

    Add 404 Page

---
+++ 404.php
@@ -0,0 +1,22 @@
+<?php get_header(); ?>
+
+<section class="page-interior error-page">
+
+     <div class="interior-wrap">
+
+               <article class="main-content <?php echo the_title(); ?>">    
+          
+                    <h1 class="error-page">Shoot! 404 Error</h1>
+
+                    <div class="page-content">
+                         <h2>Page Not Found</h2>
+                         <p>Sorry, the page you're looking for doesn't exist.  Please double check that you typed the URL correctly or use the navigation to find the page you're looking for!  Cheers!</p>
+                    </div>
+
+               </article><!-- /main-content -->
+
+     </div><!-- /interior-wrap -->
+
+</section><!-- /page-interior -->
+
+<?php get_footer(); ?>
