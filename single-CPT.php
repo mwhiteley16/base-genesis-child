db38dc31b6c0c2b403d93b85aa6b585fd68e5ffc
Matt Whiteley <matt@whiteleydesigns.com>
2016-11-08 14:55:58 -0500

    Single-CPT.php update

--- single-CPT.php
+++ single-CPT.php
@@ -1,24 +1,17 @@
 <?php get_header(); ?>
 
-<section class="page-interior single custom-posttype">
+<section class="page-interior">
 
      <div class="interior-wrap">
 
      <?php if (have_posts()) : while (have_posts()) : the_post();?>
 
-     <article class="main-content <?php echo the_title(); ?>">    
-
-          <h1 class="page-header"><?php the_title(); ?></h1>
-
           <div class="page-content">
+               <h1 class="page-header"><?php the_title(); ?></h1>
                <?php the_content(); ?>
           </div>
 
-     </article><!-- /main-content -->
-
      <?php endwhile; endif; ?>
-     
-     <?php get_template_part('inc/main-sidebar'); ?>
 
      </div>
 
