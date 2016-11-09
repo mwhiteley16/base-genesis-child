eee62b430bd9c88ff0c376837dd1b425cd0dbef2
Matt Whiteley <matt@whiteleydesigns.com>
2016-11-09 12:24:58 -0500

    Delete header.php

--- inc/header.php
+++
@@ -1,7 +0,0 @@
-<div class="header-logo-wrap">
-     <a href="<?php echo get_home_url(); ?>"><img class="header-logo" src="logo-source-URL" alt="LOGO ALT"></a>
-</div>
-
-<?php wp_nav_menu( array('menu' => 'Main Menu', 'container_class' => 'header-menu-wrap' )); ?>
-
-<i class="fa fa-bars mobile-menu-toggle"></i>
