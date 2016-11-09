8db60ca0459315f23d5e6aabccf02bcbc839108d
Matt Whiteley <matt@whiteleydesigns.com>
2016-11-09 12:23:16 -0500

    Create header.php in correct folder

---
+++ sections/header.php
@@ -0,0 +1,9 @@
+<div class="header-logo-wrap full-width align-center">
+     <a href="<?php echo get_home_url(); ?>"><img class="header-logo" src="http://basegenesis.whiteleydesigns.com/wp-content/uploads/2016/11/wd-basegenesis-logo.png" alt="Genesis Child Theme Logo"></a>
+</div>
+
+<?php // wp_nav_menu( array('menu' => 'Main Menu', 'container_class' => 'nav-primary' )); // Use this to pull in the main menu if you don't want to use the default 'Primary Navigation' setting ?>
+
+<p class="align-center full-width">Edit the header area using the /sections/header.php file in the base child theme.</p>
+
+<i class="fa fa-bars mobile-menu-toggle"></i>
