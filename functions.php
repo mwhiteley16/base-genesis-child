20411f0c923c8813b1dd48f78c4390b2b7a7d381
Matt Whiteley <matt@whiteleydesigns.com>
2016-11-11 14:01:43 -0500

    Activate FA CDN

--- functions.php
+++ functions.php
@@ -39,15 +39,15 @@ function wd_load_scripts() {
 //* GENERIC -- Register & Enqueue Additional Scripts
 //* Used to enqueue a variety of scripts.  Common ones are listed below, simply uncomment the action and the needed scripts to use
 //* Add additional scripts as needed
-//add_action( 'wp_enqueue_scripts', 'wd_enqueue_scripts' );
+add_action( 'wp_enqueue_scripts', 'wd_enqueue_scripts' );
 function wd_enqueue_scripts() {
      //wp_register_script( 'jquery-lazyload', get_stylesheet_directory_uri() . '/js/jquery.lazyload.js', array('jquery'), '1.9.3', true ); // lazyload
-	//wp_enqueue_script( 'jquery-lazyload' );
-     //wp_register_script( 'font-awesome-cdn', 'https://use.fontawesome.com/c312c6a5ae.js' ); // font awesome (CDN)
-	//wp_enqueue_script( 'font-awesome-cdn' );
+     //wp_enqueue_script( 'jquery-lazyload' );
+     wp_register_script( 'font-awesome-cdn', 'https://use.fontawesome.com/c312c6a5ae.js' ); // font awesome (CDN)
+     wp_enqueue_script( 'font-awesome-cdn' );
      //wp_register_script( 'flickity', get_stylesheet_directory_uri() . '/js/flickity.pkgd.min.js', array('jquery'), '1.9.3', true ); // flickity
-	//wp_enqueue_script( 'flickity' );
-	//wp_register_script('wd-slideshow', get_stylesheet_directory_uri() . '/js/wd-slideshow.js' ); // WD Slideshow
+     //wp_enqueue_script( 'flickity' );
+     //wp_register_script('wd-slideshow', get_stylesheet_directory_uri() . '/js/wd-slideshow.js' ); // WD Slideshow
      //wp_enqueue_script( 'wd-slideshow' );
 }
 
