4ce74bb81d055f7bc17b5b8cb185b184c2731138
Matt Whiteley <matt@whiteleydesigns.com>
2017-12-20 12:37:23 -0500

    Cleanup.

--- archive-CPT.php
+++ archive-CPT.php
@@ -1,35 +1,24 @@
 <?php get_header(); ?>
 
 <section class="page-interior">
-
      <div class="interior-wrap">
-
-     <article class="main-content">    
-
-          <h1 class="page-header">CPT Name</h1>
-
           <div class="page-content">
 
-          <?php $item_count = 0; ?>
-          <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-          <?php $item_count++; ?>
-               
-               <div class="archive-item archive-item-<?php echo $item_count; ?>">
-                    <?php the_post_thumbnail(); ?>
-                    <div class="acrhive-item-right">
-                         <h2><?php the_title(); ?></h2>
-                         <div class="archive-content"><?php the_content(); ?></div>
-                    </div><!-- /archive-item-right -->
-               </div><!-- /archive-item -->
+               <h1 class="page-header">CPT Name</h1>
+               <?php $item_count = 0; ?>
+               <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                    <?php $item_count++; ?>                    
+                    <div class="archive-item archive-item-<?php echo $item_count; ?>">
+                         <?php the_post_thumbnail(); ?>
+                         <div class="acrhive-item-right">
+                              <h2><?php the_title(); ?></h2>
+                              <div class="archive-content"><?php the_content(); ?></div>
+                         </div>
+                    </div>     
+               <?php endwhile; endif; ?>
           
-          <?php endwhile; endif; ?>
-          
-          </div><!-- /page-content -->
-
-     </article><!-- /main-content -->
-
+          </div>
      </div>
-
-</section><!-- /page-interior -->
+</section>
 
 <?php get_footer(); ?>
