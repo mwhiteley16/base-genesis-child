ac3e1a2d0fcd84e017ff599c29903dc407d6082a
Matt Whiteley <matt@whiteleydesigns.com>
2017-12-20 12:39:19 -0500

    Cleanup.

--- category.php
+++ category.php
@@ -1,15 +1,12 @@
 <?php get_header(); ?>
 
 <section class="page-interior">
-
      <div class="interior-wrap">
-
-          <h1 class="page-header"><?php single_cat_title(); ?></h1>
-
-          <?php genesis_loop(); ?>
-
-     </div><!-- /interior-wrap -->
-
-</section><!-- /page-interior -->
+          <div class="page-content">
+               <h1 class="page-header"><?php single_cat_title(); ?></h1>
+               <?php genesis_loop(); ?>
+          </div>
+     </div>
+</section>
 
 <?php get_footer(); ?>
