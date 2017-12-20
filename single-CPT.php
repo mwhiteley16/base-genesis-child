f1c87585f0a37d0a14d361d8d9397831b8f47378
Matt Whiteley <matt@whiteleydesigns.com>
2017-12-20 12:38:30 -0500

    Cleanup.

--- single-CPT.php
+++ single-CPT.php
@@ -1,20 +1,14 @@
 <?php get_header(); ?>
 
+<?php if (have_posts()) : while (have_posts()) : the_post();?>
 <section class="page-interior">
-
      <div class="interior-wrap">
-
-     <?php if (have_posts()) : while (have_posts()) : the_post();?>
-
           <div class="page-content">
                <h1 class="page-header"><?php the_title(); ?></h1>
                <?php the_content(); ?>
           </div>
-
-     <?php endwhile; endif; ?>
-
      </div>
-
-</section><!-- /page-interior -->
+</section>
+<?php endwhile; endif; ?>
 
 <?php get_footer(); ?>
