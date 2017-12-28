fe8c705be79e0fb33c1059883124af022250da25
Matt Whiteley <matt@whiteleydesigns.com>
2017-12-28 14:00:37 -0500

    Update category.php

--- category.php
+++ category.php
@@ -4,7 +4,7 @@
      <div class="interior-wrap">
           <div class="page-content">
                <h1 class="page-header"><?php single_cat_title(); ?></h1>
-               <?php genesis_loop(); ?>
+               <?php do_action('genesis_loop'); ?>
           </div>
      </div>
 </section>
