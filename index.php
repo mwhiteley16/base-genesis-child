547142ec44c1cdf008b249e95e6182faad9625b4
Matt Whiteley <matt@whiteleydesigns.com>
2018-01-26 13:44:49 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,14 @@
 <section class="page-interior">
      <div class="interior-wrap">
           <div class="page-content">
-               <h1 class="page-header">Blog</h1>
+               <?php if( is_author() ) : ?>
+                    <?php $curauth = (isset($_GET['author_name'])) ? get_user_by('slug', $author_name) : get_userdata(intval($author)); ?>
+                    <h1 class="page-header">Posts by <?php echo $curauth->display_name; ?></h1>
+               <?php elseif( is_category() ) : ?>
+                    <h1 class="page-header">Category: <?php single_cat_title(); ?></h1>
+               <?php else : ?>
+                    <h1 class="page-header">Blog</h1>
+               <?php endif; ?>
                <?php do_action('genesis_loop'); ?>
           </div>
      </div>
